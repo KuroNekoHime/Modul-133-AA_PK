1376623876cb37357622c41ffb0e846a31bf64d2
AlexA <alexander.ackermann@gmx.ch>
2015-12-22 08:30:48 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML>
+<!-- Website Template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Animaniac</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<div class="header">
+			<ul>
+				<li class="selected">
+					<a href="index.html">home</a>
+				</li>
+				<li>
+					<a href="about.html">about</a>
+				</li>
+				<li>
+					<a href="blog.html">blog</a>
+				</li>
+				<li>
+					<a href="shows.html">shows</a>
+				</li>
+			</ul>
+			<form action="index.html">
+				<input type="text" name="email" id="email" value="Enter e-mail address">
+				<input type="submit" name="submit" id="submit" value="">
+			</form>
+		</div>
+		<div class="body">
+			<a href="index.html" class="logo"><img src="images/logo.png" alt=""></a>
+			<a class="download" href="#"></a>
+		</div>
+		<div class="footer">
+
+		</div>
+	</div>
+	<div id="body">
+		<div class="content">
+			<a href="#"><img src="images/lifestyle.jpg" alt=""></a>
+			<p>
+				Wer will schon lange auf den neusten Manga oder Anime warten? Du sicherlich nicht! Also nimm deine Pokébälle raus und krall dir die neusten, Top aktuellsten Animes, Mangas und Merchandise.
+			</p>
+			<p>
+				Bist du ein wahrer Otaku? Hast du bereits die sieben Dragonballs gefunden und Arceus gefangen? Wenn nicht solltest du schleunigst damit anfangen!
+				Falls dies aber nicht der Fall ist, möchte ich dir etwas zeigen was dich sicherlich interessieren könnte. <a href="products.php">Klick mich!</a>
+			</p>
+		</div>
+		<div class="sidebar">
+			<div>
+				<h3>neuste</h3>
+				<ul>
+					<li>
+						<a href="shows.html">Ousama Game&nbsp;&nbsp;-&nbsp;&nbsp;<b>Band 1-3</b></a>
+					</li>
+					<li>
+						<a href="shows.html">Magical Girl of the End&nbsp;&nbsp;-&nbsp;&nbsp;<b>Band 1-5</b></a>
+					</li>
+					<li>
+						<a href="shows.html">The World God only knows&nbsp;&nbsp;-&nbsp;&nbsp;<b>Band 1</b></a>
+					</li>
+					<li>
+						<a href="shows.html">Sword Art Online&nbsp;&nbsp;-&nbsp;&nbsp;<b>Staffel 1 + 2</b></a>
+					</li>
+				</ul>
+			</div>
+			<div class="connect">
+				<h3>social</h3>
+				<a href="https://plus.google.com/" id="googleplus">googleplus</a>
+				<a href="https://www.facebook.com/" id="facebook">facebook</a>
+				<a href="https://twitter.com/?lang=de" id="twitter">twitter</a>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<p>
+			&copy;Copyright 2015. <a href="index.html">Nekoji AG</a> all rights reserved
+		</p>
+	</div>
+</body>
+</html>
