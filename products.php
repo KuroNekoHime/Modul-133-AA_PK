be8f8cf9fce02feb6e41c9a7a1e3118221a40e14
AlexA <alexander.ackermann@gmx.ch>
2015-12-22 08:31:43 +0100

    Create products.php

---
+++ products.php
@@ -0,0 +1,131 @@
+<!DOCTYPE HTML>
+<!-- Website Template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Products - Animaniac</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<div class="header">
+			<ul>
+				<li>
+					<a href="index.html">home</a>
+				</li>
+				<li>
+					<a href="about.html">about</a>
+				</li>
+				<li class="selected">
+					<a href="blog.html">blog</a>
+				</li>
+				<li>
+					<a href="shows.html">shows</a>
+				</li>
+			</ul>
+			<form action="index.html">
+				<input type="text" name="email" id="email" value="Enter e-mail address">
+				<input type="submit" name="submit" id="submit" value="">
+			</form>
+		</div>
+		<div class="body">
+			<a href="index.html" class="logo"><img src="images/logo.png" alt=""></a>
+		</div>
+		<div class="footer">
+			Hier kommen die ein kleines Gerede hin
+		</div>
+	</div>
+	<div id="body">
+		<div class="content">
+			<h2>Artikel übersicht</h2>
+			<div>
+				<h3>Artikel 1</h3>
+				<a href="#"><img src="images/johannedoe2.jpg" alt=""></a>
+				<p>
+					Beschreibung zu Artikel.
+				</p>
+				<a href="#" class="more">see more</a>
+			</div>
+			<div>
+				<h3>Artikel 2</h3>
+				<a href="#"><img src="images/johannedoe-sideview.jpg" alt=""></a>
+				<p>
+					Beschreibung zu Artikel.<br>
+					Es kommen mehr Artikel auf eine solche Seite.<br>
+					Immer jeweils alles von einem, also alle Bände von Ousama Game usw.
+				</p>
+				<a href="#" class="more">see more</a>
+			</div>
+			<div class="paging">
+				<ul>
+					<li class="first">
+						<a href="#">first</a>
+					</li>
+					<li class="selected">
+						<a href="#">1</a>
+					</li>
+					<li>
+						<a href="#">2</a>
+					</li>
+					<li>
+						<a href="#">3</a>
+					</li>
+					<li>
+						<a href="#">4</a>
+					</li>
+					<li>
+						<a href="#">5</a>
+					</li>
+					<li>
+						<a href="#">6</a>
+					</li>
+					<li>
+						<a href="#">7</a>
+					</li>
+					<li>
+						<a href="#">8</a>
+					</li>
+					<li>
+						<a href="#">9</a>
+					</li>
+					<li>
+						<a href="#">10</a>
+					</li>
+					<li>
+						<a href="#">11</a>
+					</li>
+					<li>
+						<a href="#">12</a>
+					</li>
+					<li>
+						<a href="#">13</a>
+					</li>
+					<li>
+						<a href="#">14</a>
+					</li>
+					<li>
+						<a href="#">15</a>
+					</li>
+					<li>
+						<a href="#">16</a>
+					</li>
+					<li>
+						<a href="#">17</a>
+					</li>
+					<li class="last">
+						<a href="#">last</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="sidebar">
+			<img src="images/test.jpg" alt="" border="2px">
+		</div>
+	</div>
+	<div id="footer">
+		<p>
+			&copy;Copyright 2023. <a href="index.html">Company name</a> all rights reserved
+		</p>
+	</div>
+</body>
+</html>
